ee83cb146e6ed98bd472722135726955b8915e2b
Artem <art.sha03@gmail.com>
2023-10-20 21:19:41 +0200

    Added basic models for Card and Transaction

---
+++ Primat-ATM/Model/Card.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primat_ATM.Model
+{
+    class Card : INotifyPropertyChanged
+    {
+        private int cardId;
+        private string cardNumber;
+        private float balance;
+        private string email;
+        private string pin;
+        public int CardId
+        {
+            get
+            {
+                return cardId;
+            }
+            set
+            {
+                cardId = value;
+                OnPropertyChanged("CardId");
+            }
+        }
+
+        public string CardNumber
+        {
+            get
+            {
+                return cardNumber;
+            }
+            set
+            {
+                cardNumber = value;
+                OnPropertyChanged("CardNumber");
+            }
+        }
+
+        public float Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+                OnPropertyChanged("Balance");
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        public string Pin
+        {
+            get
+            {
+                return pin;
+            }
+            set
+            {
+                pin = value;
+                OnPropertyChanged("Pin");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Primat-ATM/Model/Transaction.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primat_ATM.Model
+{
+    class Transaction : INotifyPropertyChanged
+    {
+        private int transactionId;
+        private DateTime timestamp;
+        private int fromId;
+        private int toId;
+        private float amount;
+
+        public int TransactionId
+        {
+            get
+            {
+                return transactionId;
+            }
+            set
+            {
+                transactionId = value;
+                OnPropertyChanged("TransactionId");
+            }
+        }
+
+        public DateTime Timestamp
+        {
+            get
+            {
+                return timestamp;
+            }
+            set
+            {
+                timestamp = value;
+                OnPropertyChanged("Timestamp");
+            }
+        }
+
+        public int FromId
+        {
+            get
+            {
+                return fromId;
+            }
+            set
+            {
+                fromId = value;
+                OnPropertyChanged("FromId");
+            }
+        }
+
+        public int ToId
+        {
+            get
+            {
+                return toId;
+            }
+            set
+            {
+                toId = value;
+                OnPropertyChanged("ToId");
+            }
+        }
+
+        public float Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+                OnPropertyChanged("Amount");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
