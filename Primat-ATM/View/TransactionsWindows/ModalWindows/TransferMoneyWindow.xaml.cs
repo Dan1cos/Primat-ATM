9ecc9324e50b9074566386b951d9623fb9a67e9c
polis <polishchuk.maks@gmail.com>
2023-10-29 19:53:35 +0200

    Додано вікно TransferMoneyWindow та дію на кнопці Balance.

---
+++ Primat-ATM/View/TransactionsWindows/ModalWindows/TransferMoneyWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Primat_ATM.View.TransactionsWindows.ModalWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для TransferMoneyWindow.xaml
+    /// </summary>
+    public partial class TransferMoneyWindow : Window
+    {
+        public TransferMoneyWindow(string hedline_card)
+        {
+            InitializeComponent();
+
+            HeadLine_CardNumber.Text = hedline_card;
+        }
+
+        private void GoBack(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
