ac6961f2e1b41538628c17502e29e269f22016e1
Artem <art.sha03@gmail.com>
2023-11-12 23:16:59 +0100

    Added email and PIN change

--- Primat-ATM/ViewModel/ChangeEmailViewModel.cs
+++ Primat-ATM/ViewModel/ChangeEmailViewModel.cs
@@ -1,21 +1,53 @@
-﻿using Primat_ATM.ViewModel.Services;
+﻿using Primat_ATM.Model;
+using Primat_ATM.Repository;
+using Primat_ATM.View;
+using Primat_ATM.ViewModel.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Primat_ATM.ViewModel
 {
     public class ChangeEmailViewModel:ViewModelBase
     {
+        private string _email;
+        private const string _match = @"[-A-Za-z0-9!#$%&'*+/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?";
+        private string _errorMessage;
         private INavigationService _navigationService;
+        private ICardRepository CardRepository;
+        public ICardService CardService { get; }
+        private Regex regex;
         public RelayCommand NavigateCancelCommand { get; set; }
-        public ChangeEmailViewModel(INavigationService navigationService)
+        public RelayCommand ChangeEmailCommand { get; set; }
+        private ConfirmationDialog _confirmEmail;
+        public ChangeEmailViewModel(INavigationService navigationService, ICardService cardService)
         {
+            CardRepository = new CardRepository();
             NavigationService = navigationService;
+            CardService = cardService;
+            Regex regex = new Regex(_match);
+            _confirmEmail = new ConfirmationDialog();
 
             NavigateCancelCommand = new RelayCommand(o => { NavigationService.NavigateTo<SettingsViewModel>(); }, o => true);
+            ChangeEmailCommand = new RelayCommand(ExecuteChangeEmailCommand, CanExecuteChangeEmailCommand);
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged("ErrorMessage");
+            }
         }
 
         public INavigationService NavigationService
@@ -26,6 +58,30 @@ namespace Primat_ATM.ViewModel
                 _navigationService = value;
                 OnPropertyChanged();
             }
+        }
+
+        private void ExecuteChangeEmailCommand(object obj)
+        {
+            _confirmEmail.ConfirmationDilog("Are you sure you want to change the email?");
+            if (_confirmEmail.ShowCustomDilog())
+            {
+                if (CanExecuteChangeEmailCommand(obj))
+                {
+                    ErrorMessage = "";
+                    CardService.Card.Email = _email;
+                    CardRepository.Edit(CardService.Card);
+                    NavigationService.NavigateTo<SettingsViewModel>();
+                }
+                else
+                {
+                    ErrorMessage = "Incorrect email";
+                }
+            }
+
+        }
+        private bool CanExecuteChangeEmailCommand(object obj)
+        {
+            return !string.IsNullOrWhiteSpace(Email) && Email.Length <=45 && Regex.Match(Email, _match).Success;
         }
     }
 }
--- Primat-ATM/ViewModel/ChangePasswordViewModel.cs
+++ Primat-ATM/ViewModel/ChangePasswordViewModel.cs
@@ -1,21 +1,57 @@
-﻿using Primat_ATM.ViewModel.Services;
+﻿using Primat_ATM.Model;
+using Primat_ATM.Repository;
+using Primat_ATM.View;
+using Primat_ATM.ViewModel.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Primat_ATM.ViewModel
 {
     public class ChangePasswordViewModel: ViewModelBase
     {
+        Card _card;
+        private string _repeatNewPin;
+        private string _errorMessage;
         private INavigationService _navigationService;
+        private ICardRepository CardRepository;
+        public ICardService CardService { get; set; }
         public RelayCommand NavigateCancelCommand { get; set; }
-        public ChangePasswordViewModel(INavigationService navigationService)
+        public ICommand ChangePasswordCommand { get; }
+        private ConfirmationDialog _confirmPin;
+        public ChangePasswordViewModel(INavigationService navigationService, ICardService cardService)
         {
+            CardRepository = new CardRepository();
             NavigationService = navigationService;
 
+            CardService = cardService;
+            _card = new Card();
+            _confirmPin = new ConfirmationDialog();
             NavigateCancelCommand = new RelayCommand(o => { NavigationService.NavigateTo<SettingsViewModel>(); }, o => true);
+            ChangePasswordCommand = new RelayCommand(ExecuteChangePasswordCommand, CanExecuteChangePasswordCommand);
+        }
+
+        public string NewPin
+        {
+            get => _card.Pin;
+            set => _card.Pin = value;
+        }
+        public string RepeatNewPin
+        {
+            get => _repeatNewPin;
+            set => _repeatNewPin = value;
+        }
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged("ErrorMessage");
+            }
         }
 
         public INavigationService NavigationService
@@ -26,6 +62,32 @@ namespace Primat_ATM.ViewModel
                 _navigationService = value;
                 OnPropertyChanged();
             }
+        }
+
+        private void ExecuteChangePasswordCommand(object obj)
+        {
+            _confirmPin.ConfirmationDilog("Are you sure you want to change the password?");
+            if (_confirmPin.ShowCustomDilog())
+            {
+                if (CanExecuteChangePasswordCommand(obj))
+                {
+                    CardService.Card.Pin = _card.Pin;
+                    CardRepository.Edit(CardService.Card);
+                    NavigationService.NavigateTo<SettingsViewModel>();
+                }
+                else
+                {
+                    ErrorMessage = "Password is either empty or is not 4 digits";
+                }
+            }
+
+        }
+        private bool CanExecuteChangePasswordCommand(object obj)
+        {
+            return !string.IsNullOrWhiteSpace(NewPin) && NewPin.Length == 4 && NewPin.All(char.IsDigit) &&
+                !string.IsNullOrWhiteSpace(RepeatNewPin) && RepeatNewPin.Length == 4 && RepeatNewPin.All(char.IsDigit) &&
+                RepeatNewPin.Equals(NewPin)
+                ? true : false;
         }
     }
 }
