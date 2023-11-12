617eccbd33cf3bf2ec3ace34e9e1f8c31357671f
Artem <art.sha03@gmail.com>
2023-11-12 22:21:16 +0100

    Updated card numbers display

--- Primat-ATM/ViewModel/StatementViewModel.cs
+++ Primat-ATM/ViewModel/StatementViewModel.cs
@@ -9,11 +9,19 @@ using System.Threading.Tasks;
 
 namespace Primat_ATM.ViewModel
 {
+    /*public class EnhancedTransaction
+    {
+        public DateTime Timestamp;
+        public string From;
+        public string To;
+        public float Amount;
+    }*/
     public class StatementViewModel: ViewModelBase
     {
         private int _selectedComboBoxId = 0;
         private INavigationService _navigationService;
         private ITransactionRepository transactionRepository;
+        private ICardRepository cardRepository;
         private IList<Transaction> _transactionList;
         public RelayCommand NavigateCancelCommand { get; set; }
         public ICardService CardService { get; set; }
@@ -22,10 +30,26 @@ namespace Primat_ATM.ViewModel
             NavigationService = navigationService;
             CardService = cardService;
             transactionRepository = new TransactionRepository();
+            cardRepository = new CardRepository();
             TransactionList = transactionRepository.GetByToId(CardService.Card.CardId);
 
             NavigateCancelCommand = new RelayCommand(o => { NavigationService.NavigateTo<TransactionsViewModel>(); }, o => true);
         }
+
+        /*public IList<EnhancedTransaction> convertToEnhanced(IList<Transaction> transactions)
+        {
+            IList<EnhancedTransaction> enhancedTransactions = new List<EnhancedTransaction>();
+            foreach (Transaction transaction in transactions)
+            {
+                EnhancedTransaction trans = new EnhancedTransaction();
+                trans.Timestamp = transaction.Timestamp;
+                trans.To = cardRepository.GetById(transaction.ToId).CardNumber;
+                trans.From = cardRepository.GetById(transaction.FromId).CardNumber;
+                trans.Amount = transaction.Amount;
+                enhancedTransactions.Add(trans);
+            }
+            return enhancedTransactions;
+        }*/
 
         public IList<Transaction> TransactionList
         {
@@ -57,10 +81,10 @@ namespace Primat_ATM.ViewModel
                 switch (_selectedComboBoxId)
                 {
                     case 1:
-                        TransactionList = transactionRepository.GetByFromId(CardService.Card.CardId);
+                        TransactionList = transactionRepository.GetByToId(CardService.Card.CardId);
                         break;
                     default:
-                        TransactionList = transactionRepository.GetByToId(CardService.Card.CardId);
+                        TransactionList = transactionRepository.GetByFromId(CardService.Card.CardId);
                         break;
                 }
             }
